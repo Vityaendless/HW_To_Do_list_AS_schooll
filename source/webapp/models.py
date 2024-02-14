a956d956bf036ed36daa8188534a630e420160fd
Vityaendless <dvitya031190@gmail.com>
2024-02-14 17:22:44 +0600

    Fix status in Task model

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -8,7 +8,7 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 class Task(models.Model):
     description = models.CharField(max_length=100, null=True, blank=True, verbose_name='Описание')
-    status = models.CharField(null=True, blank=True, verbose_name='Статус', choices=status_choices)
+    status = models.CharField(max_length=30, null=True, blank=True, verbose_name='Статус', choices=status_choices)
     deadline = models.CharField(max_length=10, null=False, blank=False, verbose_name='Дата окончания')
     created_at = models.DateTimeField(verbose_name="Время создания", auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
