f2ec0c7d91f52c5f13aa7877a0107ce87c98a73e
Vityaendless <dvitya031190@gmail.com>
2024-02-14 18:01:45 +0600

    Add create task functional

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from .models import Task
+from django.http import  HttpResponseRedirect
+from .models import Task, status_choices
 
 
 # Create your views here.
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'index.html', {'tasks': tasks})
+
+def new(request):
+    if request.method == 'GET':
+        return render(request, 'new_task.html', {'statuses': status_choices})
+    elif request.method == 'POST':
+        Task.objects.create(
+            description=request.POST.get('description'),
+            status=request.POST.get('status'),
+            deadline=request.POST.get('deadline')
+        )
+        return HttpResponseRedirect('/')
