8e5f99c3eab426d4e8294390d98333fec3c0488a
Vityaendless <dvitya031190@gmail.com>
2024-02-14 17:19:15 +0600

    Create task model and enabled work with it in admin page

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'created_at']
+    list_display_links = ['id', 'description']
+    list_filter = ['created_at', 'status']
+    search_fields = ['description']
+    fields = ['description', 'status', 'deadline', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+
+
+class Task(models.Model):
+    description = models.CharField(max_length=100, null=True, blank=True, verbose_name='Описание')
+    status = models.CharField(null=True, blank=True, verbose_name='Статус', choices=status_choices)
+    deadline = models.CharField(max_length=10, null=False, blank=False, verbose_name='Дата окончания')
+    created_at = models.DateTimeField(verbose_name="Время создания", auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+    def __str__(self):
+        return f"{self.id}, {self.description}|"
