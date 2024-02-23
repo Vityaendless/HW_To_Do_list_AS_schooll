cb2fdbc2cd3baa4d84e5d361cea6fa9ebb175345
Vityaendless <dvitya031190@gmail.com>
2024-02-23 13:13:46 +0600

    Add task create with use django forms

---
+++ source/webapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import widgets
+from .models import status_choices
+
+
+class TaskForm(forms.Form):
+    description = forms.CharField(max_length=100, required=True, label='Description')
+    full_description = forms.CharField(max_length=3000, required=True, label='Full description', widget=widgets.Textarea)
+    status = forms.ChoiceField(required=True, label='Status', widget=widgets.Select, choices=status_choices)
+    deadline = forms.CharField(max_length=10, required=True, label='Deadline')
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import  HttpResponseRedirect
-from .models import Task, status_choices
+from .models import Task
+from .forms import TaskForm
 
 
 # Create your views here.
@@ -10,15 +10,20 @@ def index(request):
 
 def new(request):
     if request.method == 'GET':
-        return render(request, 'new_task.html', {'statuses': status_choices})
+        form = TaskForm()
+        return render(request, 'new_task.html', {'form': form})
     elif request.method == 'POST':
-        task = Task.objects.create(
-            description=request.POST.get('description'),
-            full_description=request.POST.get('full_description'),
-            status=request.POST.get('status'),
-            deadline=request.POST.get('deadline')
-        )
-        return redirect('view', pk=task.pk)
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = Task.objects.create(
+                description=request.POST.get('description'),
+                full_description=request.POST.get('full_description'),
+                status=request.POST.get('status'),
+                deadline=request.POST.get('deadline')
+            )
+            return redirect('view', pk=task.pk)
+        else:
+            return render(request, 'new_task.html', {'form': form})
 
 
 def view(request, *args, pk):
