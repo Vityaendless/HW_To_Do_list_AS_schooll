01650980ca92593f0d1aab12c099dd5885b00dd8
Vityaendless <dvitya031190@gmail.com>
2024-03-17 22:22:26 +0600

    add index and view task

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from .views import IndexView, TaskView
+
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('task/<int:pk>/', TaskView.as_view(), name='task_view'),
+]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View, TemplateView
+from .models import Task
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
+
+
+class TaskView(TemplateView):
+    template_name = 'task_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=kwargs.get('pk'))
+        return context
