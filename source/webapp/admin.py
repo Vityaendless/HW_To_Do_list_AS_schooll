69edca90a690f55efed63bcf11c7cab127e59c31
Vityaendless <dvitya031190@gmail.com>
2024-03-17 21:03:01 +0600

    add models

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Task, Type, Status
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id','summary', 'created_at']
+    list_display_links = ['id', 'summary']
+    list_filter = ['summary', 'description', 'created_at']
+    search_fields = ['summary', 'description']
+    fields = ['summary', 'description', 'type', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+    list_filter = ['title']
+    search_fields = ['title']
+    fields = ['title']
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+    list_filter = ['title']
+    search_fields = ['title']
+    fields = ['title']
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class AbstractModel(models.Model):
+    created_at = models.DateTimeField(verbose_name="Время создания", auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+    class Meta:
+        abstract = True
+
+
+class Task(AbstractModel):
+    summary = models.CharField(max_length=50, verbose_name="Заголовок")
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Описание")
+    type = models.ForeignKey('webapp.Type', related_name='tasks', on_delete=models.PROTECT, verbose_name='Тип')
+    status = models.ForeignKey('webapp.Status', related_name='tasks', on_delete=models.PROTECT, verbose_name='Статус')
+
+    def __str__(self):
+        return f"Task {self.summary}"
+
+
+class Type(AbstractModel):
+   title = models.CharField(max_length=40, verbose_name='Название')
+
+   def __str__(self):
+       return self.title
+
+
+class Status(AbstractModel):
+    title = models.CharField(max_length=40, verbose_name='Название')
+
+    def __str__(self):
+        return self.title
