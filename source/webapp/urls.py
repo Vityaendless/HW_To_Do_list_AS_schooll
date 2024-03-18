13d42c30411db6804c244128898a1467de557b01
Vityaendless <dvitya031190@gmail.com>
2024-03-18 20:25:33 +0600

    add update

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView, TaskView, TaskCreateView
+from .views import IndexView, TaskView, TaskCreateView, TaskUpdateView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('task/<int:pk>/', TaskView.as_view(), name='task_view'),
     path('articles/add/', TaskCreateView.as_view(), name='new_task'),
+    path('article/<int:pk>/update/', TaskUpdateView.as_view(), name='update_task'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -39,3 +39,28 @@ class TaskCreateView(View):
             return redirect('task_view', pk=task.pk)
         else:
             return render(request, 'new_task.html', {'form': form})
+
+
+class TaskUpdateView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=kwargs.get('pk'))
+        form = TaskForm(initial={
+            'summary': task.summary,
+            'description': task.description,
+            'type': task.type,
+            'status': task.status
+        })
+        return render(request, 'update_task.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=kwargs.get('pk'))
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task.summary=form.cleaned_data.get('summary')
+            task.description=form.cleaned_data.get('description')
+            task.type=form.cleaned_data.get('type')
+            task.status = form.cleaned_data.get('status')
+            task.save()
+            return redirect('task_view', pk=task.pk)
+        else:
+            return render(request, 'update_task.html', {'form': form})
