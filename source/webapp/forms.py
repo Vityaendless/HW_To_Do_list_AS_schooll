7d18f32649811590251c2a4befecb9a950c9e57c
Vityaendless <dvitya031190@gmail.com>
2024-03-20 21:25:33 +0600

    Added validation

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms import widgets
+from django.core.exceptions import ValidationError
 from .models import status_choices
 
 
@@ -8,3 +9,16 @@ class TaskForm(forms.Form):
     full_description = forms.CharField(max_length=3000, label='Full description', widget=widgets.Textarea)
     status = forms.ChoiceField(required=True, label='Status', widget=widgets.Select, choices=status_choices)
     deadline = forms.CharField(max_length=10, label='Deadline')
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if len(description) < 5:
+            raise ValidationError('Need length more than 5 symbols')
+        return description
+
+    def clean(self):
+        description = self.cleaned_data.get('description')
+        full_description = self.cleaned_data.get('full_description')
+        if description == full_description:
+            raise ValidationError('Description and full description couldnt be equal')
+        return super().clean()
