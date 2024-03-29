c52e1e3d5b8a650edd30144d66b974483b7b8acc
Vitya Endless <dvitya031190@gmail.com>
2024-03-30 00:01:06 +0600

    orm queries

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -3,6 +3,9 @@ from django.views.generic import View, TemplateView, FormView
 from .models import Task
 from .forms import TaskForm
 
+from django.db.models import Q, F
+from datetime import datetime, timedelta
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -10,6 +13,20 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.all()
+        #1
+        #context['tasks'] = Task.objects.filter(updated_at__date__gte=datetime.now().date() - timedelta(days=30), status__title__iexact='done')
+        #2
+        #context['tasks'] = Task.objects.filter((Q(status__title='Done') | Q(status__title='New')) & (Q(types__title='Task') | Q(types__title='Bug')))
+        #3
+        #context['tasks'] = Task.objects.filter(Q(types__title='Bug') | Q(summary__contains='bug'))
+        #4
+        #context['tasks'] = Task.objects.values('pk','id', 'summary', 'status__title', 'types__title')
+        #5
+        #context['tasks'] = Task.objects.filter(summary=F('description'))
+        #6
+        #context['tasks'] = Task.objects.filter(types__title='Task').count()
+        #context['tasks'] = Task.objects.filter(types__title='Bug').count()
+        #context['tasks'] = Task.objects.filter(types__title='Enhancement').count()
         return context
 
 
