4b2acf10fd61aab440da7e26c76f8c5bf59083bd
Vitya Endless <dvitya031190@gmail.com>
2024-03-29 22:32:10 +0600

    final fixes

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -6,7 +6,7 @@ from .views import IndexView, TaskView, TaskCreateView, \
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('task/<int:pk>/', TaskView.as_view(), name='task_view'),
-    path('tasks/add/', TaskCreateView.as_view(), name='new_task'),
-    path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='update_task'),
-    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='delete_task'),
+    path('task/add/', TaskCreateView.as_view(), name='new_task'),
+    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='update_task'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='delete_task'),
 ]
