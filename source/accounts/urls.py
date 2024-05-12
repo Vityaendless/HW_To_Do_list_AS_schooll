376ef8951810447218d2480ba23a14ba8d8b0395
Vitya Endless <dvitya031190@gmail.com>
2024-05-12 19:51:50 +0600

    Task 1 is done - bonus 1

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView
+from django.contrib.auth.views import LogoutView
+from .views import RegisterView, MyLoginView
 
 
 app_name = 'accounts'
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', MyLoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', RegisterView.as_view(), name='registration'),
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import redirect, reverse
+from django.shortcuts import redirect, reverse, HttpResponseRedirect
 from django.contrib.auth import login, get_user_model
+from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 from .forms import NewUserForm
+
+
+class MyLoginView(LoginView):
+    def dispatch(self, request, *args, **kwargs):
+        next_page = self.request.GET.get('next')
+        reg_url = "/accounts/registration/"
+        login_url = "/accounts/login/"
+        if next_page:
+            if reg_url in next_page or login_url in next_page:
+                return HttpResponseRedirect('/accounts/login/')
+        return super().dispatch(request, *args, **kwargs)
 
 
 class RegisterView(CreateView):
     model = get_user_model()
     form_class = NewUserForm
     template_name = 'registration.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        next_page = self.request.GET.get('next')
+        reg_url = "/accounts/registration/"
+        login_url = "/accounts/login/"
+        if next_page:
+            if login_url in next_page or reg_url in next_page:
+                return HttpResponseRedirect('/accounts/registration/')
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         user = form.save()
